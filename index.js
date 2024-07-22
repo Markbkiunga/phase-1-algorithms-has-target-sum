e1a570d3940297411612c1fc2d08cef2e837b404
Mark Brian <markbkiunga2017@gmail.com>
2024-07-22 15:00:38 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+/*My Code
 function hasTargetSum(array, target) {
   // Write your algorithm here
   let i = 0;
-  for (i = 0; i < array.length; i++) {
-    let x = array[0];
-    let y = array[i + 1];
+  for (let j = 0; j < array.length; j++) {
+    let x = array[i];
+    let y = array[j + 1]; //Not supposed to add the number itself
     let sum = x + y;
     if (sum === target) {
-      console.log(true);
       return true;
+    } else {
+      let newX = array[i + 1];
+      let newY = array[i + 1 + 1];
+      let newSum = newX + newY;
+      if (newSum === target) {
+        return true;
+      }
     }
   }
-  console.log(false);
   return false;
 }
-hasTargetSum([1, 2, 5], 7);
+
+console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
+*/
+function hasTargetSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+// console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
+
 /* 
   Write the Big O time complexity of your function here
 */
@@ -21,33 +42,47 @@ hasTargetSum([1, 2, 5], 7);
 /* 
   Add your pseudocode here
   START
-  ADD NUMBER 1 TO OTHERS AND RETURN TRUE IF SUM = TARGET
-  ELSE ADD NUMBER 2 TO OTHERS AND RETURN TRUE IF SUM = TARGET
-  ELSE ADD NUMBER 3 TO OTHERS AND RETURN TRUE IF SUM = TARGET
-  ...
-  ELSE RETURN FALSE
+  Iterate over array
+  Let complementary = Target - Number
+  If there is complementary in the rest of the array
+  return true
+  Else return false
+
   STOP
 */
 
 /*
   Add written explanation of your solution here
+  Using for loop we iterate over the elements performing the subtraction of the current element from the target
+  We assign the result from the operation above to complement
+  We iterate over the elements which follow the 
 */
 
 // You can run `node index.js` to view these console logs
-// if (require.main === module) {
-// add your own custom tests in here
-//   console.log('Expecting: true');
-//   console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-//   console.log('');
+  console.log('');
 
-//   console.log('Expecting: true');
-//   console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([22, 19, 4, 6, 30], 25));
 
-//   console.log('');
+  console.log('');
 
-//   console.log('Expecting: false');
-//   console.log('=>', hasTargetSum([1, 2, 5], 4));
-// }
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([1, 2, 5], 4));
 
-// module.exports = hasTargetSum;
+  console.log('');
+
+  console.log('Expecting: false');
+  console.log('=>', hasTargetSum([4], 4));
+
+  console.log('');
+
+  console.log('Expecting: true');
+  console.log('=>', hasTargetSum([-1, 2, 7, 4], 6));
+}
+
+module.exports = hasTargetSum;
